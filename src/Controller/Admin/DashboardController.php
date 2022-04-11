e4d9849403a58f08d032d1c4e40013e3ff0058b0
d.arishev <sividmitry@gmail.com>
2022-04-11 17:46:01 +0300

    Parsing setup

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,20 +4,34 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use App\Entity\Seller;
+use App\Form\ParsingRequestFormType;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Controller\ParserController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/parser', name: 'admin')]
-    public function index(): Response
+    public function show(Request $request, ParserController $parsing): Response
     {
+        $form = $this->createForm(ParsingRequestFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $result = $parsing->collectData($form->getData());
+            dd($result);
+        }
 
-        return $this->render('admin/index.html.twig');
+
+        return $this->render('admin/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
@@ -43,10 +57,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
-            yield MenuItem::linkToCrud('Sellers', 'fas fa-map-marker-alt', Seller::class);
-        yield MenuItem::linkToCrud('Products', 'fas fa-comments', Product::class);
+        yield MenuItem::linkToDashboard('Parser', 'fa fa-paste');
+        // yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
+        //    yield MenuItem::linkToCrud('Sellers', 'fas fa-map-marker-alt', Seller::class);
+        // yield MenuItem::linkToCrud('Products', 'fas fa-comments', Product::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
--- src/Controller/ParserController.php
+++ src/Controller/ParserController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp;
+use function MongoDB\BSON\toRelaxedExtendedJSON;
 
 class ParserController extends AbstractController
 {
@@ -13,5 +16,19 @@ class ParserController extends AbstractController
         return $this->render('parser/index.html.twig', [
             'controller_name' => 'ParserController',
         ]);
+    }
+
+    public function collectData($url)
+    {
+
+        $url = implode($url);
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', $url);
+
+        if ($res->getBody()) {
+            dd( $res->GetHeaders());
+            // JSON string: { ... }
+        }
+
     }
 }
