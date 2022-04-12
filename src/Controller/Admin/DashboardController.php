2a49fa6b248d8d33802c97c269f19fbe6e83e145
d.arishev <sividmitry@gmail.com>
2022-04-12 18:05:38 +0300

    Parser controller refactoring

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,11 +8,14 @@ use App\Form\ParsingRequestFormType;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Controller\ParserController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DomCrawler\Crawler;
 use Twig\Environment;
 
 class DashboardController extends AbstractDashboardController
@@ -20,33 +23,31 @@ class DashboardController extends AbstractDashboardController
     #[Route('/parser', name: 'admin')]
     public function show(Request $request, ParserController $parsing): Response
     {
-        $form = $this->createForm(ParsingRequestFormType::class);
+        /*     $form = $this->createForm(ParsingRequestFormType::class);
+             $form->handleRequest($request);
+             if ($form->isSubmitted()) {
+                 $result = $parsing->collectData($form->getData());
+                 dd($result);
+             }
+
+
+             return $this->render('admin/index.html.twig', [
+                 'form' => $form->createView()
+             ]);*/
+        $form = $this->createFormBuilder(null)
+            ->add('query', TextType::class)
+            ->add('search', SubmitType::class)
+            ->getForm();
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
             $result = $parsing->collectData($form->getData());
-            dd($result);
+            echo $result;
+
+
         }
-
-
         return $this->render('admin/index.html.twig', [
             'form' => $form->createView()
         ]);
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
--- src/Controller/ParserController.php
+++ src/Controller/ParserController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use GuzzleHttp;
-use function MongoDB\BSON\toRelaxedExtendedJSON;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
+use GuzzleHttp\Handler\CurlMultiHandler;
+use Symfony\Component\DomCrawler\Crawler;
+use GuzzleHttp\HandlerStack;
 
 class ParserController extends AbstractController
 {
@@ -21,14 +25,18 @@ class ParserController extends AbstractController
     public function collectData($url)
     {
 
-        $url = implode($url);
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', $url);
+        $client = new Client(); //sync client
+        $proCli = $client->getAsync('https://www.ozon.ru/category/kedy-i-slipony-muzhskie-7660/?sorting=new')->wait();
+        $crawler = new Crawler($proCli->getBody()->getContents());
+      //  $xpatch = '';
+      //  dd($crawler->filterXPath($xpatch)->outerHtml());
+        dd($crawler->outerHtml());
 
-        if ($res->getBody()) {
-            dd( $res->GetHeaders());
-            // JSON string: { ... }
-        }
+
 
     }
 }
+
+
+
+
